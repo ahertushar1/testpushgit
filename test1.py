8ff403ac3c24967e8670688dcb6f057b299c59ec
tushar <ahertushar1@gmail.com>
2022-07-31 10:50:23 +0530

    this is my first code push in git

--- test1.py
+++ test1.py
@@ -11,3 +11,28 @@ d = {
 db1 = client['mongotest']
 coll = db1['test']
 coll.insert_one(d )
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
+d = {
+    "name":"sudhanshu",
+    "email" : "sudhanshu@ineuron.ai",
+    "surname" : "kumar"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d )
+
